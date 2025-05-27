22ba7285fd342a7b31fcbbfe90d5647fb329ac4e
Basil Fierz <basil.fierz@hotmail.com>
2025-05-27 21:58:08 +0200

    Install dart as post installation step

---
+++ scripts/post_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y apt-transport-https
+sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
+sudo apt-get update
+sudo apt-get install -y dart
+export PATH="$PATH:/usr/lib/dart/bin"
+source ~/.bashrc
+
+dart --version
+rustc --version
